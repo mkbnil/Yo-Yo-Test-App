3b9477d5b364c9a8fe04885bede431a6f4b34ec9
mkbnil <cnctbinil@gmail.com>
2020-10-17 14:47:23 +0530

    Basic DataHandler Completed
    Basic DataHandler Completed

--- YoYoTestApp/YoYoTestApp/TestDataHandler.cs
+++ YoYoTestApp/YoYoTestApp/TestDataHandler.cs
@@ -47,7 +47,24 @@ namespace YoYoTestApp
             }
         }
 
+        public async void OnWarningButtonClick(int id)
+        {
+            _athletes.Where(i => i.Id == id && i.IsWarned == true).ToList().ForEach(i => i.IsStopped = true);
+            _athletes.Where(i => i.Id == id && i.IsWarned == false).ToList().ForEach(i => i.IsWarned = true);
+        }
+        public async void OnStopButtonClick(int id)
+        {
+            _athletes.Where(i => i.Id == id && i.IsStopped == false).ToList().ForEach(i => i.IsStopped = true);  
+        }
 
+        public async void  OnEditButtonClick(int id, int level, int shuttle)
+        {
+            foreach (var athlete in _athletes.Where(i => i.Id == id))
+            {
+                athlete.Level = level;
+                athlete.Shuttle = shuttle;
+            }
+        }
         private async void Run(TestData testdata)
         {
             var shuttleno = 0;
@@ -65,8 +82,18 @@ namespace YoYoTestApp
                     if (_shuttleTimeLeft == 0) break;
                     await Task.Delay(1000);
                 }
+                UpdateAthleteInfo();
             }
 
+        }
+
+        private void UpdateAthleteInfo()
+        {
+            foreach(var athlete in _athletes.Where(i => i.IsStopped == false))
+            {
+                athlete.Level = _level;
+                athlete.Shuttle = _shuttle;
+            }
         }
 
         private void LoadAthletes()
@@ -80,7 +107,7 @@ namespace YoYoTestApp
         {
             _tests = JsonConvert.DeserializeObject<List<TestData>>(File.ReadAllText("./fitnessrating_beeptest.json"));
         }
-                
+
         private void UpdateCounter()
         {
             _shuttleTimeElapsed += 1;
